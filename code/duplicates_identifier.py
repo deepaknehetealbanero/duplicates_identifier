d6325274bcc1fe6e296b5824eae5bd1fc2f1cbb5
deepaknehetealbanero <deepak.nehete@albanero.io>
2023-07-03 12:20:00 +0530

    Duplicates Identifier - changes few script names

--- code/duplicates_identifier.py
+++ code/duplicates_identifier.py
@@ -1,16 +1,15 @@
 from pyspark.sql.functions import *
 from pyspark.sql.functions import lit, col, when
 from pyspark.sql import SparkSession
-from functions import mathFunctions
 import time
 
-in_parms = dict(in_path = '/home/albanero/PycharmProjects/in-de-codepair-template-python/data/',
+in_parms = dict(in_path = '/home/albanero/PycharmProjects/duplicates_identifier/data/',
                 in_file_1 = 'in_data.csv')
-out_parms = dict(out_path = '/home/albanero/PycharmProjects/in-de-codepair-template-python/data_out/')
+out_parms = dict(out_path = '/home/albanero/PycharmProjects/duplicates_identifier/data_out/')
 
 def get_list_of_dup_col():
     dup_cols = {}
-    conf_file = open('../config/duplicate_column_list.conf', "r")
+    conf_file = open('../config/duplicate_identifier_columns.conf', "r")
     for line in conf_file:
         k, v = line.split("=")
         dup_cols[k] = v
@@ -24,8 +23,7 @@ def main():
     print (dup_cols)
     in_data, spark = read_data(in_parms)
     in_data.createTempView("employee_data")
-    query = "Select phone, email, count(phone) as count_phone from employee_data " + " group by " + dup_cols['duplicate_col1'] + ", "\
-            + dup_cols['duplicate_col2'] +  " having count_phone > 1"
+    query = "Select phone, email, count(phone) as count_phone from employee_data " + " group by " + dup_cols['duplicate_col1'] + ", " + dup_cols['duplicate_col2'] +  " having count_phone > 1"
     dup_data = spark.sql(query)
     out_data = in_data.join(dup_data, in_data.phone == dup_data.phone).select(in_data.first_name, in_data.last_name, in_data.gender, in_data.date_of_birth, in_data.address, in_data.city, in_data.state, in_data.country, in_data.email, in_data.phone)
     out_data.show(20, False)
